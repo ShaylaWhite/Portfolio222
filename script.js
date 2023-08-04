3a70fb77fbe198ca5e73c08729cf4a3727dba436
Shayla White <shayladwhite1@gmail.com>
2023-08-04 16:03:01 -0700

    width

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const buttons = document.querySelectorAll(".card-buttons button");
-const sections = document.querySelectorAll(".card-section");
-const card = document.querySelector(".card");
+const cards = document.querySelectorAll(".card");
 
-const handleButtonClick = (e) => {
-  const targetSection = e.target.getAttribute("data-section");
-  const section = document.querySelector(targetSection);
-  targetSection !== "#about"
-    ? card.classList.add("is-active")
-    : card.classList.remove("is-active");
-  card.setAttribute("data-state", targetSection);
-  sections.forEach((s) => s.classList.remove("is-active"));
-  buttons.forEach((b) => b.classList.remove("is-active"));
-  e.target.classList.add("is-active");
-  section.classList.add("is-active");
+const handleCardHover = (e) => {
+  if (e.target.classList.contains("card")) {
+    e.target.classList.add("glow");
+    e.target.addEventListener("animationend", () => {
+      e.target.classList.remove("glow");
+    }, { once: true });
+  }
 };
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", handleButtonClick);
+cards.forEach((card) => {
+  card.addEventListener("mouseover", handleCardHover);
+});
+
+const expandButton = document.querySelector(".expand-button");
+const cardContent = document.querySelector(".card-content");
+const portfolioItemDetails = document.querySelectorAll(".portfolio-item-details");
+
+expandButton.addEventListener("click", () => {
+  cardContent.classList.toggle("expanded");
+  portfolioItemDetails.forEach((item) => item.classList.toggle("expanded"));
 });
